f77df063f9bfd028832796910ca7e915a3962504
FakirHerif <basdemir35@hotmail.com.tr>
2023-12-20 00:43:00 +0300

    routes and handler functions were defined in a simple structure

--- backend/cmd/api-server/main.go
+++ backend/cmd/api-server/main.go
@@ -1,12 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 
 	"github.com/FakirHerif/Support-Ticket-Service/backend/internal/model"
 )
 
 func main() {
-	fmt.Println("Hello")
+
 	model.Model()
+
+	r := gin.Default()
+
+	v1 := r.Group("/api")
+	{
+		v1.GET("informations", getInformations)
+		v1.GET("informations/:id", getInformationsByID)
+		v1.POST("informations", addInformations)
+		v1.PUT("informations/:id", updateInformationsByID)
+		v1.DELETE("informations/:id", deleteInformationsByID)
+	}
+
+	r.Run()
+}
+
+func getInformations(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "SUCCESS: All Informations Was Received"})
+}
+
+func getInformationsByID(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "SUCCESS: Information Was Received"})
+}
+
+func addInformations(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "SUCCESS: Informations Added"})
+}
+
+func updateInformationsByID(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "SUCCESS: Informations Changed"})
+}
+
+func deleteInformationsByID(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "SUCCESS: Informations Deleted"})
 }

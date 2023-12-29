c7580a1a2c50b4bd557e36fb6055106539bbb291
FakirHerif <basdemir35@hotmail.com.tr>
2023-12-29 06:48:30 +0300

    FormSend codes were edited and the style file for this component was added

--- frontend/ticket-support/src/components/FormSend.jsx
+++ frontend/ticket-support/src/components/FormSend.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
-import axios from 'axios';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useAuth } from './AuthContext';
 import { useNavigate } from 'react-router-dom';
 import { useFormContext } from './FormContext';
+import '../components/basicstyle/form.css';
 
 const FormSend = () => {
 
     const { updateFormValues, updateReferenceID } = useFormContext();
-
-    const { user } = useAuth();
-
+    const { user, axios } = useAuth();
     const navigate = useNavigate(); 
 
     const initialValues = {
@@ -88,38 +86,21 @@ const FormSend = () => {
             const formData = new FormData();
             formData.append('jsonData', JSON.stringify(jsonData));
 
-                  
-            const response = await axios.post('http://localhost:8080/api/informations', formData, {
+            const response = await axios.post('/informations', formData, {
               headers: {
                 'Content-Type': 'multipart/form-data',
-                Authorization: 'Bearer YOUR_TOKEN_HERE',
               },
             });
-    
-          console.log('Form submitted:', response.data);
-          console.log('Form details:', values);
-
-console.log("***********************************");
-          console.log('Attachments:', values.attachments);
-console.log(user)
-
-    
-
-         
-    
-          toast.success('Form submitted successfully!', { autoClose: 3000 });
-          console.log(response.data.referenceID);
-          updateFormValues(values);
-          updateReferenceID(response.data.referenceID);
-          navigate(`/basvuru-basarili/`);
-          
-        } catch (error) {
-          console.error('Form submission failed:', error);
-          console.log('Form submitted:', error.values);
-          console.log('Failed form values:', values);
-          toast.error('Form submission failed!', { autoClose: 3000 });
-        }
-      };
+              toast.success('Form submitted successfully!', { autoClose: 3000 });
+              updateFormValues(values);
+              updateReferenceID(response.data.referenceID);
+              navigate(`/basvuru-basarili/`);
+            } catch (error) {
+                console.error('Form submission failed:', error);
+                console.log('Failed form values:', values);
+                toast.error('Form submission failed!', { autoClose: 3000 });
+              }
+        };  
     
       return (
         <Formik
@@ -136,185 +117,169 @@ console.log(user)
             handleSubmit,
             setFieldValue,
           }) => (
-          <div className='login'>
-            <div className='form'>
-            <form noValidate onSubmit={handleSubmit} encType="multipart/form-data">
+          <div className='sendform'>
+            <div className='formsend'>
+              <form noValidate onSubmit={handleSubmit} encType="multipart/form-data">
                 <span>Send Form</span>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.firstName}
-              placeholder="Enter Firstname"
-              className="form-control inp_text"
-            />
-            <p className="error">
-                {errors.firstName && touched.firstName && <div>{errors.firstName}</div>}
-            </p>
-            
 
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.lastName}
-              placeholder="Enter Lastname"
-              className="form-control inp_text"
-            />
-            <p className="error">
-                {errors.lastName && touched.lastName && <div>{errors.lastName}</div>}
-            </p>
+                <div className="grid-container">
+                  <div className="grid-item-name">
+                    <input
+                    type="text"
+                    id="firstName"
+                    name="firstName"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.firstName}
+                    placeholder="Enter Firstname"
+                    className="form-control inp_text"
+                    />
+                    <p className="error">{errors.firstName && touched.firstName && <div>{errors.firstName}</div>}</p>
+                  </div>
+                  <div className="grid-item-lastname">
+                    <input
+                    type="text"
+                    id="lastName"
+                    name="lastName"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.lastName}
+                    placeholder="Enter Lastname"
+                    className="form-control inp_text"
+                    />
+                    <p className="error">{errors.lastName && touched.lastName && <div>{errors.lastName}</div>}</p>
+                  </div>
+                </div>
 
-            <input
-              type="number"
-              id="age"
-              name="age"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.age}
-              placeholder="Enter Age"
-              className="form-control inp_number"
-            />
-            <p className="error">
-                {errors.age && touched.age && <div>{errors.age}</div>}
-            </p>
+                <div className="grid-containerTwo">
+                  <div className="grid-item-identification">
+                    <input
+                    type="number"
+                    id="identificationNo"
+                    name="identificationNo"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.identificationNo}
+                    placeholder="Enter Identity No"
+                    className="form-control inp_number"
+                    />
+                    <p className="error">{errors.identificationNo && touched.identificationNo && <div>{errors.identificationNo}</div>}</p>
+                  </div>
+                  <div className="grid-item-age">
+                    <input
+                    type="number"
+                    id="age"
+                    name="age"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.age}
+                    placeholder="Enter Age"
+                    className="form-control inp_number"
+                    />
+                    <p className="error">{errors.age && touched.age && <div>{errors.age}</div>}</p>
+                  </div>
+                </div>
 
-            <input
-              type="number"
-              id="identificationNo"
-              name="identificationNo"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.identificationNo}
-              placeholder="Enter Identity No"
-              className="form-control inp_number"
-            />
-            <p className="error">
-                {errors.identificationNo && touched.identificationNo && <div>{errors.identificationNo}</div>}
-            </p>
+                <input
+                type="text"
+                id="address"
+                name="address"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.address}
+                placeholder="Enter Address"
+                className="form-control inp_text"
+                />
+                <p className="error">{errors.address && touched.address && <div>{errors.address}</div>}</p>
 
+                <div className="grid-container">
+                  <div className="grid-item-city">
+                    <input
+                      type="text"
+                      id="city"
+                      name="city"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.city}
+                      placeholder="Enter City"
+                      className="form-control inp_text"
+                    />
+                    <p className="error">{errors.city && touched.city && <div>{errors.city}</div>}</p>
+                  </div>
+                  <div className="grid-item-town">
+                    <input
+                    type="text"
+                    id="town"
+                    name="town"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.town}
+                    placeholder="Enter Town"
+                    className="form-control inp_text"
+                    />
+                    <p className="error">{errors.town && touched.town && <div>{errors.town}</div>}</p>
+                  </div>
+                </div>
+                
+                <input
+                type="text"
+                id="phone"
+                name="phone"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.phone}
+                placeholder="Enter Phone Number"
+                className="form-control inp_text"
+                />
+                <p className="error">{errors.phone && touched.phone && <div>{errors.phone}</div>}</p>
+                
+                <input
+                type="text"
+                id="title"
+                name="title"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.title}
+                placeholder="Enter Form Title"
+                className="form-control inp_text"
+                />
+                <p className="error">{errors.title && touched.title && <div>{errors.title}</div>}</p>
 
-            <input
-              type="text"
-              id="address"
-              name="address"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.address}
-              placeholder="Enter Address"
-              className="form-control inp_text"
-            />
-            <p className="error">
-                {errors.address && touched.address && <div>{errors.address}</div>}
-            </p>
+                <textarea
+                type='text'
+                id="content"
+                name="content"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.content}
+                placeholder="Enter Form Content"
+                className="form-control inp_text"
+                ></textarea>
+                <p className="error">{errors.content && touched.content && <div>{errors.content}</div>}</p>
 
-
-
-
-            <input
-              type="text"
-              id="city"
-              name="city"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.city}
-              placeholder="Enter City"
-              className="form-control inp_text"
-            />
-            <p className="error">
-                {errors.city && touched.city && <div>{errors.city}</div>}
-            </p>
-
-
-            <input
-              type="text"
-              id="town"
-              name="town"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.town}
-              placeholder="Enter Town"
-              className="form-control inp_text"
-            />
-            <p className="error">
-                {errors.town && touched.town && <div>{errors.town}</div>}
-            </p>
-
-
-            <input
-              type="text"
-              id="phone"
-              name="phone"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.phone}
-              placeholder="Enter Phone Number"
-              className="form-control inp_text"
-            />
-            <p className="error">
-                {errors.phone && touched.phone && <div>{errors.phone}</div>}
-            </p>
-
-
-            <input
-              type="text"
-              id="title"
-              name="title"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.title}
-              placeholder="Enter Form Title"
-              className="form-control inp_text"
-            />
-            <p className="error">
-                {errors.title && touched.title && <div>{errors.title}</div>}
-            </p>
-
-
-            <input
-              type="text"
-              id="content"
-              name="content"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.content}
-              placeholder="Enter Form Content"
-              className="form-control inp_text"
-            />
-            <p className="error">
-                {errors.content && touched.content && <div>{errors.content}</div>}
-            </p>
-
-            
-            <input
-              type="file"
-              id="attachments"
-              name="attachments"
-              onChange={(event) => {
-                const selectedFile = event.target.files[0]; // Sadece ilk seçilen dosyayı al
-                if (event.target.files.length > 1) {
-                  event.target.value = null; // Birden fazla dosya seçildiyse input değerini sıfırla
-                  setFieldValue('attachments', null); // Attachments değerini null yap
-                } else {
-                  setFieldValue('attachments', selectedFile ? [selectedFile] : null); // Tek dosya seçildiyse attachments'e ekle, aksi halde null yap
-                }
-              }}
-              multiple
-              className="form-control inp_file"
-            />
-
-
-           
-          <button type="submit">Submit</button>
-        </form>
-    </div>
-    </div>
-      )}
-    </Formik>
+                <input
+                type="file"
+                id="attachments"
+                name="attachments"
+                onChange={(event) => {
+                  const selectedFile = event.target.files[0]; // Get only the first selected file
+                  if (event.target.files.length > 1) {
+                    event.target.value = null; // Reset input value if more than one file is selected
+                    setFieldValue('attachments', null); // Make Attachments value null
+                  } else {
+                    setFieldValue('attachments', selectedFile ? [selectedFile] : null); // If a single file is selected, add it to attachments, otherwise set it to null
+                  }
+                }}
+                multiple
+                className="form-control inp_file"
+                />
+                
+                <button type="submit">Submit</button>
+              </form>
+            </div>
+          </div>
+          )}
+        </Formik>
       );
     };
 
